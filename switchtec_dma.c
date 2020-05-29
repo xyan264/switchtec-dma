be99a24e4e6bf3708d0fcf687a4f486f740f1e22
Kelvin Cao <kelvin.cao@microchip.com>
2020-05-29 13:58:31 +0800

    Add fabric API is_switchtec_fabric()
    to test if a DMA channel is a switchtec DMA channel.
    
    Fix a bug in is_switchtec_fabric()

--- switchtec_dma.c
+++ switchtec_dma.c
@@ -2031,6 +2031,17 @@ bool is_fabric_dma(struct dma_device *dma)
 	return false;
 }
 
+bool is_switchtec_fabric(struct dma_chan *chan)
+{
+	struct switchtec_dma_chan *c;
+	list_for_each_entry(c, &chan_list, list)
+		if (chan == &c->dma_chan)
+			return c->is_fabric;
+
+	return false;
+}
+EXPORT_SYMBOL(is_switchtec_fabric);
+
 struct dma_device *switchtec_fabric_get_dma_device(char *name)
 {
 	struct switchtec_dma_dev *d;
